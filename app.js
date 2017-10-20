c8e58d73a5fb5fa5c9414b70b4abab303f25cbe9
Chris Williams <chrwilliams@csumb.edu>
2017-10-20 04:55:48 +0000

    Updated view engine to handlebars instead of mustache. Similar engine, but more compliant with express.

--- app.js
+++ app.js
@@ -11,16 +11,27 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
-var mustacheExpress = require('mustache-express');
+// var mustacheExpress = require('express-handlebars');
 
-// Register '.mustache' extension with The Mustache Express
-app.engine('mustache', mustacheExpress());
+// // Register '.mustache' extension with The Mustache Express
+// app.engine('mustache', mustacheExpress({
+//   extname: 'mustache', 
+//   defaultLayout: 'layout'
+// }));
 
-app.set('view engine', 'mustache');
+var hbs = require("express-handlebars");
+
+app.engine('hbs', hbs({
+    extname: 'hbs',
+    defaultLayout: 'layout',
+    layoutsDir: __dirname + '/views/layouts/'
+}));
+
+app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
