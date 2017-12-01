307d20ff33f4fbe568a26d8e4d2a9b1c792cbd69
Mathias Torgalsboeen <mtorgalsboeen@csumb.edu>
2017-12-01 23:53:34 +0000

    Added app use for morgan, express, bodyparser, methodoverride

--- server.js
+++ server.js
@@ -22,6 +22,20 @@ global.yelp = new Yelp({
   app_secret: 'u8MZcqH1I44frxrsPeemgksM8RnYCplcrsyBCHNRrEsqST0RGxOnKNNne3uhuyaF'
 })
 
+app.use(express.static(__dirname + '/public')); 				// set the static files location /public/img will be /img for users
+app.use(morgan('dev')); 										// log every request to the console
+app.use(bodyParser.urlencoded({'extended':'true'})); 			// parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); 									// parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(methodOverride());
+
+// routes ======================================================================
+require('./app/routes.js')(app);
+
+// listen (start app with node server.js) ======================================
+app.listen(port);
+console.log("App listening on port " + port);
+
 //
 // ## SimpleServer `SimpleServer(obj)`
 //
