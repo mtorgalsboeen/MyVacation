b16327143b20b7ca74e9135fd689a75f91578749
Chris Williams <chrwilliams@csumb.edu>
2017-12-07 08:27:53 +0000

    Added vacation data to manage page, fixed links on index page to redirect to manage page, and fixed createVacation cancel button.

--- routes/manage.js
+++ routes/manage.js
@@ -1,13 +1,41 @@
 var express = require('express');
 var router = express.Router();
 
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('manage', { 
-      title: 'Mangae vacation'
-  });
+    res.render('manage', {
+        title: 'Manage Vacation'
+    });
+});
+
+router.get('/:vacationId', function(req, res, next) {
+    User.findOne({ "userToken": req.session.userToken }).exec(function(err, user) {
+        if (err) {
+            res.send(JSON.stringify({
+                error: "Error finding user"
+            }));
+        }
+        else {
+            // We have a user, check if there is a vacation with supplied id
+            var vacation = user.vacations.id(req.params.vacationId);
+            if (vacation != null) { // If so, update and save
+                res.render('manage', {
+                    title: 'Manage Vacation',
+                    vacation: JSON.stringify(vacation), // Send the entire vacation (for later)
+                    vacationTitle: vacation.vacationTitle   // Send the vacationTitle separate for ease of use
+                });
+            }
+            else {
+                res.send(JSON.stringify({
+                    error: "Vacation not found"
+                }));
+            }
+        }
+    });
 });
 
 module.exports = router;
